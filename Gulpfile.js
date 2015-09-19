11b586b06e20336b1db86a621c9f518b93570d80
Molly Kent <kent.molly@gmail.com>
2015-09-19 12:58:02 -0700

    Reconfigure Gulpfile and file structure. Make button border style classes

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function() {
-    gulp.src("./src/sass/*.scss")
+    gulp.src("public/sass/**/*.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("./public/css/"));
 });
 
-gulp.task("default", function() {
-    gulp.watch("./src/sass/*.scss", ["sass"]);
+gulp.task("watch", ["sass"], function() {
+  gulp.watch("public/sass/**/*.scss", ["sass"]);
 });
