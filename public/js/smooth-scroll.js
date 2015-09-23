cc43d2e07fab273848dec4ce19d2b9ca24f4fa48
Alina To <alina.to@gmail.com>
2015-09-22 18:13:17 -0700

    Modify IDs in HTML. No appropriate place for #blog-nav

--- public/js/smooth-scroll.js
+++ public/js/smooth-scroll.js
@@ -4,15 +4,15 @@ $('.menu a').on('click', function(e) {
   e.preventDefault();
   var target = this.hash;
   var $target = $(target);
-  var targetOffset = $target.offset().top-55;
-
+  var $targetOffset = $target.offset().top-80;
+  // top-80 is for where the scroll stops
+  // offset from top by 80px
   $('html, body').stop().animate({
-      'scrollTop': target.offset
+      'scrollTop': $targetOffset
   }, 1600, 'swing', function () {
       window.location.hash = target;
   });
 });
 
 
-
-// Source: http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+// Adapted from: http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
